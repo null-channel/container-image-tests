e98c1d53ef4ed33fb9bbe0b1d3892b8eb9d3e105
Marek Counts <countsmarek@gmail.com>
2020-12-21 12:14:43 -0500

    updated port
    Signed-off-by: Marek Counts <countsmarek@gmail.com>

--- server.go
+++ server.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/headers", headers)
-	http.ListenAndServe(":8090", nil)
+	http.ListenAndServe(":8080", nil)
 }
 
 var count = 0
